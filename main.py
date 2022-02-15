1f82e4ac63eafa3d4e1f0ffe48eee0cd202cff3d
Mads Tinko Madsen <mtm@itklyngen.dk>
2022-02-15 14:35:33 +0100

    code pretify and name changes

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import commonTools
 import getters
 import sched, time
 
-
-# Parameters
-inOctsArray = []
-outOctsArray = []
 # Sets our target device (our switch in this case).
 target = "192.168.1.1"
 # Sets the oids codes.
@@ -16,30 +12,32 @@ get_out_octs = "1.3.6.1.2.1.2.2.1.16.3"
 # Sets our credentials to the community we created on the switch.
 credentials = hlapi.CommunityData('ciscolab')
 
-def meassureBits():
+
+def meassure_bits():
     # Gets
     get_bites = (getters.get(target, [get_in_octs], credentials))
     result = commonTools.get_value(get_bites)
     return result
 
-bits = []
-graphbits = []
 
+bits = []
+graph_bits = []
 
 s = sched.scheduler(time.time, time.sleep)
+
+
 def calculate_throughput(sc):
     print("Calculating")
-    bits.append(meassureBits())
+    bits.append(meassure_bits())
     if len(bits) == 2:
-        throughputBitsPerSecond = (((bits[1] - bits[0])*8)) / 5
-        print(str(throughputBitsPerSecond) + " bps")
+        throughput_bits_per_second = ((bits[1] - bits[0]) * 8) / 5
+        print(str(throughput_bits_per_second) + " bps")
         bits[0] = bits[1]
         bits.pop(1)
-        graphbits.append(throughputBitsPerSecond)
-
+        graph_bits.append(throughput_bits_per_second)
 
     s.enter(5, 1, calculate_throughput, (sc,))
 
+
 s.enter(5, 1, calculate_throughput, (s,))
 s.run()
-
