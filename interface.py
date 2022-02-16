bb876052b45429138707dd6f4c9c2ef5670ca26d
Mads Tinko Madsen <mtm@itklyngen.dk>
2022-02-16 08:33:34 +0100

    Fixed interface names

--- interface.py
+++ interface.py
@@ -1,5 +1,5 @@
 class Interface:
-    def __init__(self, port_name, in_oct, out_oct):
+    def __init__(self, port_name):
         self.port_name = port_name
-        self.in_oct = in_oct
-        self.out_oct = out_oct
+        self.in_oct = []
+        self.out_oct = []
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sched
+import time
+
 from pysnmp import hlapi
+
 import commonTools
 import getters
-import sched, time
+import interface
+from interface import Interface
 
 # Sets our target device (our switch in this case).
 target = "192.168.1.1"
@@ -15,28 +20,36 @@ get_out_octs = "1.3.6.1.2.1.2.2.1.16.3"
 credentials = hlapi.CommunityData('ciscolab')
 
 
-def interface_information():
-    # Gets
-    get_info = (getters.get(target, [get_in_octs, get_port_name], credentials))
+def interface_information(mib_code):
+    get_info = getters.get(target, [mib_code], credentials)
     result = commonTools.get_value(get_info)
     return result
 
 
-bits = []
-graph_bits = []
+pi1 = Interface(interface_information(get_port_name))
+print("Measuring on interface " + pi1.port_name)
+bytes_in_r1 = []
+bytes_out_r1 = []
 
 s = sched.scheduler(time.time, time.sleep)
 
 
 def calculate_throughput(sc):
-    print("Calculating")
-    bits.append(interface_information())
-    if len(bits) == 2:
-        throughput_bits_per_second = ((bits[1] - bits[0]) * 8) / 5
-        print(str(throughput_bits_per_second) + " bps")
-        bits[0] = bits[1]
-        bits.pop(1)
-        graph_bits.append(throughput_bits_per_second)
+    bytes_in_r1.append(interface_information(get_in_octs))
+    if len(bytes_in_r1) == 2:
+        throughput_bits_per_second = ((bytes_in_r1[1] - bytes_in_r1[0]) * 8) / 5
+        print(str(throughput_bits_per_second) + " bps input")
+        bytes_in_r1[0] = bytes_in_r1[1]
+        bytes_in_r1.pop(1)
+        pi1.in_oct.append(throughput_bits_per_second)
+
+    bytes_out_r1.append(interface_information(get_out_octs))
+    if len(bytes_out_r1) == 2:
+        throughput_bits_per_second = ((bytes_out_r1[1] - bytes_out_r1[0]) * 8) / 5
+        print(str(throughput_bits_per_second) + " bps output")
+        bytes_out_r1[0] = bytes_out_r1[1]
+        bytes_out_r1.pop(1)
+        pi1.out_oct.append(throughput_bits_per_second)
 
     s.enter(5, 1, calculate_throughput, (sc,))
 
